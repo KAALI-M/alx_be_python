3bec44c5db0ca884089a5e416424973f54caa6d5
KAALI Mohammed <kaali.mohammed@outlook.com>
2024-06-24 22:20:39 +0100

    first commt of arrithmitic ops

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1 = 10
+number2 = 5
+print("sum of ", number1, " and ", number2, " is ", number1+number2)
+print("substraction of ", number1, " and ", number2, " is ", number1-number2)
+print("multiplication of ", number1, " and ", number2, " is ", number1*number2)
