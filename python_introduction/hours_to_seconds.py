3da8dc3a99dd37e362bd00e7f961d3f5b39b4c31
KAALI Mohammed <kaali.mohammed@outlook.com>
2024-06-24 22:43:56 +0100

    add of task 3 hours to seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,3 @@
+hours = 2
+seconds = hours*3600
+print ( hours, " hour(s) is ", seconds, "seconds"
---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,5 @@
+length = 10 
+width = 5
+area = length*width
+print (The area of the rectangle is: , area)
+
