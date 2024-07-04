1b29a76fe2a9985875a7d80fbfcb7f5df0fd383c
KAALI-M <163420046+KAALI-M@users.noreply.github.com>
2024-07-04 17:43:37 +0100

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,12 @@
 def perform_operation(num1, num2, operation):
     r = 0
-    match z : 
-        case "add" : r= num1+num2
-        case "subtract" : r= num1-num2
-        case "multiply" : r= num1*num2
-        case "divide" : r= num1/num2
+    if operation == "add" : r= num1+num2
+    elif operation ==  "subtract" : r= num1-num2
+    elif operation ==  "multiply" : r= num1*num2
+    elif operation ==  "divide" :
+        if num2 == 0 : print("division by 0")
+        else  : r= num1/num2
     return r
 
-print( perform_operation(4,5,"add"))
+print( perform_operation(4,0,"divide"))
 
