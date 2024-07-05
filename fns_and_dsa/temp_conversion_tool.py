2bdf591d0946ac35c3ad327c8d0bddf20caf1390
KAALI-M <163420046+KAALI-M@users.noreply.github.com>
2024-07-05 01:26:44 +0100

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,7 +2,7 @@ FAHRENHEIT_TO_CELSIUS_FACTOR =  5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    return (FAHRENHEIT_TO_CELSIUS_FACTOR-32)*fahrenheit
+    return (fahrenheit-32)*FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
     return celsius*CELSIUS_TO_FAHRENHEIT_FACTOR+32
