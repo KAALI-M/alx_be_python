8922a0ec246ac11a40d84644167c3ee6665af1c3
KAALI Mohammed <kaali.mohammed@outlook.com>
2024-07-11 21:30:33 +0100

    test

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount :
+    def __init__(self,account_balance=0 ):
+        self.account_balance=account_balance
+    def deposit(self, amount):
+        self.account_balance += amount
+        print(f"Deposited: ${amount}")
+    def withdraw(self, amount):
+        if self.account_balance >= amount :
+            self.account_balance -= amount
+            print(f"Withdrew: ${amount}")
+            return True
+        else:
+            print (f"Insufficient funds.")
+            return False
+    def display_balance(self):
+        print("Current Balance: ${elf.account_balance}")
