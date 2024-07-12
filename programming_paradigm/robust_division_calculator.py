34f9c74b06ee1050a46c26b2a7db96417efd938c
KAALI-M <163420046+KAALI-M@users.noreply.github.com>
2024-07-12 23:07:31 +0100

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,8 +1,10 @@
 def safe_divide(numerator, denominator):
   try:
+
+    res = float(numerator)/float(denominator)
+    
     if isinstance(numerator,(int,float))==False or tisinstance(denominator,(int,float))==False :
       raise ValueError("errorNonNumeric")
-    res = float(numerator)/float(denominator)
     print(f"The result of the division is {res:.1f}")
   except ZeroDivisionError:
     print("Error: Cannot divide by zero.")
