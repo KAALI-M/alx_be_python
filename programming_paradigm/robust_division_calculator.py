8724a13f0726912ee00fffb1ab69a65b1bc9e8e6
KAALI-M <163420046+KAALI-M@users.noreply.github.com>
2024-07-13 14:42:06 +0100

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,15 +1,16 @@
 def safe_divide(numerator, denominator):
   try:
-
-    res = float(numerator)/float(denominator)
-    
-    if isinstance(numerator,(int,float))==False or tisinstance(denominator,(int,float))==False :
+    if isinstance(numerator,(int,float))==False or isinstance(denominator,(int,float))==False :
       raise ValueError("errorNonNumeric")
-    print(f"The result of the division is {res:.1f}")
-  except ZeroDivisionError:
-    print("Error: Cannot divide by zero.")
+    res = float(numerator)/float(denominator)
+    return (f"The result of the division is {res:.1f}")
+
   except ValueError as e:
     if str(e)=="errorNonNumeric" : 
-      print("Error: Please enter numeric values only.")
+      return ("Error: Please enter numeric values only.")
     else:
-      print("erro:r: ",e)
+      return ("erro:r: ",e)
+  except ZeroDivisionError:
+    return("Error: Cannot divide by zero.")  
+
+print(safe_divide(10.1,10.1))
