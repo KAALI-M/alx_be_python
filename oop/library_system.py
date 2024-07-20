1e167468cab5e1f40eaa67f83b9acf82d22b513b
KAALI Mohammed <kaali.mohammed@outlook.com>
2024-07-20 19:41:16 +0100

    tesss

---
+++ oop/library_system.py
@@ -0,0 +1,32 @@
+class Book:
+    def __init__(self,title,author):
+        self.author = author
+        self.title = title
+    
+class EBook(Book):
+    def __init__(self, title, author,file_size):
+        super().__init__(title, author)
+        self.file_size:int = file_size
+class PrintBook(Book):
+    def __init__(self, title, author,page_count):
+        super().__init__(title, author)
+        self.page_count : int = page_count
+
+class Library:
+    def __init__(self):
+        self.Books = []
+    def add_book(self,book):
+        self.Books.append(book)
+    def list_books(self):
+        for index,book in enumerate(self.Books):
+            print(f"book {index}: {book.title} by {book.author}")
+
+#lib = Library()
+#eBook = EBook('python is good',"kAALI", 100)
+#book= Book("hydraulic gates", "david louiz")
+#pbook = PrintBook("monkeys will gouvern", "monkey", 240)
+
+#lib.add_book(eBook)
+#lib.add_book(book)
+#lib.add_book(pbook)
+#lib.list_books()
