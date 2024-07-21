72148b254e684a3a285d30ca5b62ec7bed273dd9
KAALI Mohammed <kaali.mohammed@outlook.com>
2024-07-21 11:18:33 +0100

    teeeest

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -2,10 +2,9 @@ class Calculator:
     calculation_type = "Arithmetic Operations"
     @classmethod
     def multiply(cls, a, b):
-        print(cls.calculation_type)
+        print("Calculation type: {cls.calculation_type}")
         return a * b
 
     @staticmethod
     def add(a, b):
         return a + b
-    
