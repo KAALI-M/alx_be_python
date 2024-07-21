cc437704c63dbc5a7f37654c9a63530127e47f4b
KAALI Mohammed <kaali.mohammed@outlook.com>
2024-07-21 10:31:36 +0100

    test

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,22 @@
+import math
+class Shape:
+    def area(self):
+        pass
+    
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width=width
+
+    def area(self):
+        return self.length*self.width
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius =radius
+    def area(self):
+        return math.pi*(self.radius**2)
+    
+rec=Rectangle(100,100)
+cir=Circle(100)
+print (f" rectangle {rec.area()}, circle {cir.area()}")
